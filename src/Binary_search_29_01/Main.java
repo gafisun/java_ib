af61ec315b145e9483f997403dc38cadff879bd0
baget <gafisun01@gmail.com>
2022-01-29 12:12:01 +0300

    - Оптимизировал решение предыдущей задачи за счёт бинарного поиска

--- src/Binary_search_29_01/Main.java
+++ src/Binary_search_29_01/Main.java
@@ -9,21 +9,19 @@ public class Main {
         Integer k = Integer.parseInt(scanner.next());
         ArrayList<Integer> list_n = new ArrayList<>();
         ArrayList<Integer> list_k = new ArrayList<>();
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < n; i+=1) {
             list_n.add(scanner.nextInt());
         }
-        for (int i = 0; i < k; i++) {
+        for (int i = 0; i < k; i+=1) {
             list_k.add(scanner.nextInt());
         }
         for (int i = 0; i<k; i++){
-            if (list_n.contains(list_k.get(i))){
+            if (Collections.binarySearch(list_n, list_k.get(i)) >= 0){
                 System.out.println("YES");
             } else
             {
                 System.out.println("NO");
             }
         }
-
-
     }
 }
