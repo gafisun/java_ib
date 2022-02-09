2bbfbf5fbeac541922aea0a684ed36722ed2f406
gafisun <gafisun01@gmail.com>
2022-02-09 20:01:24 +0300

    - Решил третью задачу 08.02

--- src/the_longest_name_08_02/Main.java
+++ src/the_longest_name_08_02/Main.java
@@ -1,4 +1,11 @@
 package the_longest_name_08_02;
+//Given a list of names, display the longest name.
+//For example: N = 5
+//names[] = { "Geek", "Geeks", "Geeksfor",
+//  "GeeksforGeek", "GeeksforGeeks" }
+//
+//Output:
+//GeeksforGeeks
 
 import java.util.Scanner;
 
@@ -6,6 +13,13 @@ public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         String[] intik = in.nextLine().split(" ");
+        Integer max = 0;
+        for (int i = 0; i < intik.length; i = i + 1) {
+            if (intik[i].length() > intik[max].length()) {
+                max = i;
+            }
+        }
+        System.out.println(intik[max]);
 
     }
 }
