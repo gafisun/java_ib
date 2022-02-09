ef4fb01666b88cd7df750ca8f221903f3240fc25
gafisun <gafisun01@gmail.com>
2022-02-09 20:01:36 +0300

    - Решил третью задачу 08.02

---
+++ src/rotation_08_02/Main.java
@@ -0,0 +1,19 @@
+package rotation_08_02;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String[] nk = in.nextLine().split(" ");
+        Integer n = Integer.parseInt(nk[0]);
+        Integer k = Integer.parseInt(nk[1]);
+        String[] intik = in.nextLine().split(" ");
+        for (int i = k; i < n; i = i + 1) {
+            System.out.print(intik[i]+ " ");
+        }
+        for (int i = 0; i < k; i = i + 1) {
+            System.out.print(intik[i] + " ");
+        }
+    }
+}
